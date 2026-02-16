c1363981bc3a91b282fe9ac6d655024c3d559f00
Danil <yurov3000@gmail.com>
2026-02-16 10:24:16 +0700

    create interface RoleAssignment

---
+++ src/main/java/org/example/RoleAssignment.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface RoleAssignment {
+    String assignmentId();
+    RecUser user();
+    Role role();
+    AssignmentMetadata metadata();
+    boolean isActive();
+    String assignmentType(); // "PERMANENT" или "TEMPORARY"
+}
